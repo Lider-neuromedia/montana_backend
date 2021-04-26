951c651853095bf699cf438ec84b71959f4c5241
Jose Nieto <inge1neuro@gmail.com>
2021-04-26 14:24:24 -0500

    Corrigiendo zona horaria de la aplicación para corregir la fecha de los mensajes en PQRS.

--- app/Http/Controllers/PqrsController.php
+++ app/Http/Controllers/PqrsController.php
@@ -6,6 +6,7 @@ use App\Entities\Pqrs;
 use App\Entities\SeguimientoPqrs;
 use Illuminate\Support\Facades\Validator;
 use DB;
+use Carbon\Carbon;
 
 class PqrsController extends Controller
 {
@@ -15,10 +16,10 @@ class PqrsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request){
-       
+
         $search = $request['search'];
 
-        $pqrs = Pqrs::select('id_pqrs', 'codigo', 'fecha_registro', 'ven.name AS name_vendedor', 
+        $pqrs = Pqrs::select('id_pqrs', 'codigo', 'fecha_registro', 'ven.name AS name_vendedor',
         'ven.apellidos AS apellidos_vendedor', 'cli.name AS name_cliente', 'cli.apellidos AS apellidos_cliente', 'estado')
         ->join('users AS ven', 'vendedor', '=','ven.id')
         ->join('users AS cli', 'cliente', '=','cli.id')
@@ -28,27 +29,27 @@ class PqrsController extends Controller
         ->orWhere('cli.name', 'like', "%$search%")
         ->orWhere('cli.apellidos', 'like', "%$search%")
         ->get();
-        
+
         $response = [
             'response' => 'success',
             'status' => 200,
             'pqrs' => $pqrs
         ];
-        
+
         return response()->json($response);
-        
+
     }
 
     public function getPqrsUserSesion(){
         $user = auth()->user();
         if ($user != null) {
-            $pqrs = Pqrs::select('id_pqrs', 'codigo', 'fecha_registro', 'ven.name AS name_vendedor', 
+            $pqrs = Pqrs::select('id_pqrs', 'codigo', 'fecha_registro', 'ven.name AS name_vendedor',
             'ven.apellidos AS apellidos_vendedor', 'cli.name AS name_cliente', 'cli.apellidos AS apellidos_cliente', 'estado')
             ->join('users AS ven', 'vendedor', '=','ven.id')
             ->join('users AS cli', 'cliente', '=','cli.id')
             ->where('vendedor', '=', $user->id)
             ->get();
-            
+
             $response = [
                 'response' => 'success',
                 'status' => 200,
@@ -61,10 +62,10 @@ class PqrsController extends Controller
                 'message' => 'La sesión del usuario a finalizado.'
             ];
         }
-        
-        
+
+
         return response()->json($response, $response['status']);
-        
+
     }
 
 
@@ -111,7 +112,7 @@ class PqrsController extends Controller
 
         return response()->json($response);
 
-    }   
+    }
 
     /**
      * Display the specified resource.
@@ -120,20 +121,20 @@ class PqrsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id){
-        $pqrs = Pqrs::select('id_pqrs', 'codigo', 'fecha_registro', 'pqrs.vendedor', 'pqrs.cliente', 'ven.name AS name_vendedor', 
+        $pqrs = Pqrs::select('id_pqrs', 'codigo', 'fecha_registro', 'pqrs.vendedor', 'pqrs.cliente', 'ven.name AS name_vendedor',
             'ven.apellidos AS apellidos_vendedor', 'cli.name AS name_cliente', 'cli.apellidos AS apellidos_cliente', 'estado')
             ->join('users AS ven', 'vendedor', '=','ven.id')
             ->join('users AS cli', 'cliente', '=','cli.id')
             ->where('id_pqrs', $id)
             ->first();
-        
+
         $messages_pqrs = DB::table('seguimiento_pqrs')
                         ->select('seguimiento_pqrs.*','users.name', 'users.apellidos', 'users.rol_id')
                         ->join('users', 'usuario', '=', 'id')
                         ->where('pqrs', $id)
                         ->orderBy('seguimiento_pqrs.created_at', 'ASC')
                         ->get();
-    
+
 
         foreach ($messages_pqrs as $messages) {
             $val_rol_user = auth()->user()->rol_id;
@@ -164,7 +165,7 @@ class PqrsController extends Controller
             }
         }
         $pqrs->messages_pqrs = $messages_pqrs;
-        
+
         // Busqueda de los pedidos en base al vendedor y cliente registrados en la pqrs.
         $pedidos = DB::table('pedidos')
                     ->select('pedidos.*', 'users.name', 'users.apellidos', 'users.rol_id')
@@ -198,25 +199,25 @@ class PqrsController extends Controller
         $validator = Validator::make($request->all(), [
             'mensaje' => 'required',
             'usuario' => 'exists:App\Entities\User,id|required',
-            'pqrs' => 'exists:App\Entities\Pqrs,id_pqrs|required', 
+            'pqrs' => 'exists:App\Entities\Pqrs,id_pqrs|required',
        ]);
-        
+
         if($validator->fails()){
             return response()->json($validator->messages(), 403);
         }
-        
+
         $seguimiento = new SeguimientoPqrs;
         $seguimiento->usuario = $request['usuario'];
         $seguimiento->pqrs = $request['pqrs'];
         $seguimiento->mensaje = $request['mensaje'];
-        $seguimiento->hora = date('H:i');
-        
+        $seguimiento->hora = Carbon::now()->format('H:i');
+
         if($seguimiento->save()){
             $message_pqrs = SeguimientoPqrs::select('seguimiento_pqrs.*','users.name', 'users.apellidos', 'users.rol_id')
                         ->join('users', 'usuario', '=', 'id')
                         ->where('id_seguimiento', $seguimiento->id_seguimiento)
                         ->first();
-                        
+
             $val_rol_user = auth()->user()->rol_id;
             $message_pqrs->iniciales = substr($message_pqrs->name, 0, 1);
             $message_pqrs->iniciales .= substr($message_pqrs->apellidos, 0, 1);
@@ -250,7 +251,7 @@ class PqrsController extends Controller
                 'mensaje' => $message_pqrs
             ];
         }else{
-            
+
             $response = [
                 'response' => 'error',
                 'status' => 403,
