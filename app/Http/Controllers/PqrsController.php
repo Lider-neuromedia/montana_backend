65725a8382606635b7b1971817b2638f0c2e7476
Jose Nieto <inge1neuro@gmail.com>
2021-05-21 09:49:34 -0500

    Corrigiendo creación y validación de pqrs.

--- app/Http/Controllers/PqrsController.php
+++ app/Http/Controllers/PqrsController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
+
 use App\Entities\Pqrs;
 use App\Entities\SeguimientoPqrs;
-use Illuminate\Support\Facades\Validator;
-use DB;
 use Carbon\Carbon;
+use DB;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PqrsController extends Controller
 {
@@ -15,103 +16,110 @@ class PqrsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request){
+    public function index(Request $request)
+    {
 
         $search = $request['search'];
 
         $pqrs = Pqrs::select('id_pqrs', 'codigo', 'fecha_registro', 'ven.name AS name_vendedor',
-        'ven.apellidos AS apellidos_vendedor', 'cli.name AS name_cliente', 'cli.apellidos AS apellidos_cliente', 'estado')
-        ->join('users AS ven', 'vendedor', '=','ven.id')
-        ->join('users AS cli', 'cliente', '=','cli.id')
-        ->where('codigo', 'like', "%$search%")
-        ->orWhere('ven.name', 'like', "%$search%")
-        ->orWhere('ven.apellidos', 'like', "%$search%")
-        ->orWhere('cli.name', 'like', "%$search%")
-        ->orWhere('cli.apellidos', 'like', "%$search%")
-        ->get();
+            'ven.apellidos AS apellidos_vendedor', 'cli.name AS name_cliente', 'cli.apellidos AS apellidos_cliente', 'estado')
+            ->join('users AS ven', 'vendedor', '=', 'ven.id')
+            ->join('users AS cli', 'cliente', '=', 'cli.id')
+            ->where('codigo', 'like', "%$search%")
+            ->orWhere('ven.name', 'like', "%$search%")
+            ->orWhere('ven.apellidos', 'like', "%$search%")
+            ->orWhere('cli.name', 'like', "%$search%")
+            ->orWhere('cli.apellidos', 'like', "%$search%")
+            ->get();
 
         $response = [
             'response' => 'success',
             'status' => 200,
-            'pqrs' => $pqrs
+            'pqrs' => $pqrs,
         ];
 
         return response()->json($response);
 
     }
 
-    public function getPqrsUserSesion(){
+    public function getPqrsUserSesion()
+    {
         $user = auth()->user();
         if ($user != null) {
             $pqrs = Pqrs::select('id_pqrs', 'codigo', 'fecha_registro', 'ven.name AS name_vendedor',
-            'ven.apellidos AS apellidos_vendedor', 'cli.name AS name_cliente', 'cli.apellidos AS apellidos_cliente', 'estado')
-            ->join('users AS ven', 'vendedor', '=','ven.id')
-            ->join('users AS cli', 'cliente', '=','cli.id')
-            ->where('vendedor', '=', $user->id)
-            ->get();
+                'ven.apellidos AS apellidos_vendedor', 'cli.name AS name_cliente', 'cli.apellidos AS apellidos_cliente', 'estado')
+                ->join('users AS ven', 'vendedor', '=', 'ven.id')
+                ->join('users AS cli', 'cliente', '=', 'cli.id')
+                ->where('vendedor', '=', $user->id)
+                ->get();
 
             $response = [
                 'response' => 'success',
                 'status' => 200,
-                'pqrs' => $pqrs
+                'pqrs' => $pqrs,
             ];
-        }else{
+        } else {
             $response = [
                 'response' => 'error',
                 'status' => 403,
-                'message' => 'La sesión del usuario a finalizado.'
+                'message' => 'La sesión del usuario a finalizado.',
             ];
         }
-
 
         return response()->json($response, $response['status']);
 
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $request->validate([
             'vendedor' => 'exists:App\Entities\User,id|required',
             'cliente' => 'exists:App\Entities\User,id|required',
             'tipo' => 'required',
-            'mensaje' => 'required'
+            'mensaje' => 'required',
         ]);
 
-        $pqrs = new Pqrs;
-        $pqrs->codigo = uniqid();
-        $pqrs->fecha_registro = date('Y-m-d');
-        $pqrs->cliente = $request['cliente'];
-        $pqrs->vendedor = $request['vendedor'];
-        $pqrs->tipo = $request['tipo'];
-        $pqrs->estado = 'abierto';
+        try {
+            DB::beginTransaction();
 
-        if ($pqrs->save()) {
+            $now = Carbon::now();
+
+            $pqrs = new Pqrs;
+            $pqrs->codigo = uniqid();
+            $pqrs->fecha_registro = $now->format('Y-m-d');
+            $pqrs->cliente = $request['cliente'];
+            $pqrs->vendedor = $request['vendedor'];
+            $pqrs->tipo = $request['tipo'];
+            $pqrs->estado = 'abierto';
+            $pqrs->save();
+
             DB::table('seguimiento_pqrs')->insert([
                 'usuario' => $request['cliente'],
                 'pqrs' => $pqrs->id_pqrs,
                 'mensaje' => $request['mensaje'],
-                'hora' => time()
+                'hora' => $now->format('H:i:s'),
             ]);
-            $response = [
+
+            DB::commit();
+
+            return response()->json([
                 'response' => 'success',
                 'status' => 200,
-            ];
-        }else{
-            $response = [
+            ], 200);
+
+        } catch (\Exception $ex) {
+
+            \Log::info($ex->getMessage());
+            \Log::info($ex->getTraceAsString());
+            DB::rollBack();
+
+            return response()->json([
                 'response' => 'error',
                 'status' => 403,
                 'message' => 'Error en la creación de la PQRS.',
-            ];
+            ], 500);
+
         }
-
-        return response()->json($response);
-
     }
 
     /**
@@ -120,21 +128,21 @@ class PqrsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id){
+    public function show($id)
+    {
         $pqrs = Pqrs::select('id_pqrs', 'codigo', 'fecha_registro', 'pqrs.vendedor', 'pqrs.cliente', 'ven.name AS name_vendedor',
             'ven.apellidos AS apellidos_vendedor', 'cli.name AS name_cliente', 'cli.apellidos AS apellidos_cliente', 'estado')
-            ->join('users AS ven', 'vendedor', '=','ven.id')
-            ->join('users AS cli', 'cliente', '=','cli.id')
+            ->join('users AS ven', 'vendedor', '=', 'ven.id')
+            ->join('users AS cli', 'cliente', '=', 'cli.id')
             ->where('id_pqrs', $id)
             ->first();
 
         $messages_pqrs = DB::table('seguimiento_pqrs')
-                        ->select('seguimiento_pqrs.*','users.name', 'users.apellidos', 'users.rol_id')
-                        ->join('users', 'usuario', '=', 'id')
-                        ->where('pqrs', $id)
-                        ->orderBy('seguimiento_pqrs.created_at', 'ASC')
-                        ->get();
-
+            ->select('seguimiento_pqrs.*', 'users.name', 'users.apellidos', 'users.rol_id')
+            ->join('users', 'usuario', '=', 'id')
+            ->where('pqrs', $id)
+            ->orderBy('seguimiento_pqrs.created_at', 'ASC')
+            ->get();
 
         foreach ($messages_pqrs as $messages) {
             $val_rol_user = auth()->user()->rol_id;
@@ -146,20 +154,20 @@ class PqrsController extends Controller
                 // Si el usuario autenticado es admin o vendedor. Se pone como destinatario o addressee al vendedor.
                 if ($val_rol_user == 1 || $val_rol_user == 2) {
                     $messages->addressee = true;
-                }else{
+                } else {
                     $messages->addressee = false;
                 }
-            }else if($messages->rol_id == 3){
+            } else if ($messages->rol_id == 3) {
                 // Si el usuario autenticado es admin o vendedor. pero el mensaje lo diligencio un cliente
                 if ($val_rol_user == 1 || $val_rol_user == 2) {
                     $messages->addressee = false;
-                }else{
+                } else {
                     $messages->addressee = true;
                 }
-            }else{
+            } else {
                 if ($val_rol_user == 1 || $val_rol_user == 2) {
                     $messages->addressee = true;
-                }else{
+                } else {
                     $messages->addressee = false;
                 }
             }
@@ -168,11 +176,11 @@ class PqrsController extends Controller
 
         // Busqueda de los pedidos en base al vendedor y cliente registrados en la pqrs.
         $pedidos = DB::table('pedidos')
-                    ->select('pedidos.*', 'users.name', 'users.apellidos', 'users.rol_id')
-                    ->join('users', 'cliente', '=', 'id')
-                    ->where('vendedor', $pqrs->vendedor)
-                    ->where('cliente', $pqrs->cliente)
-                    ->get();
+            ->select('pedidos.*', 'users.name', 'users.apellidos', 'users.rol_id')
+            ->join('users', 'cliente', '=', 'id')
+            ->where('vendedor', $pqrs->vendedor)
+            ->where('cliente', $pqrs->cliente)
+            ->get();
         foreach ($pedidos as $pedido) {
             $pedido->iniciales = substr($pedido->name, 0, 1);
             $pedido->iniciales .= substr($pedido->apellidos, 0, 1);
@@ -183,7 +191,7 @@ class PqrsController extends Controller
         $response = [
             'response' => 'success',
             'status' => 200,
-            'pqrs' => $pqrs
+            'pqrs' => $pqrs,
         ];
 
         return response()->json($response);
@@ -195,14 +203,15 @@ class PqrsController extends Controller
      * @param  Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function NewMessage(Request $request){
+    public function NewMessage(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'mensaje' => 'required',
             'usuario' => 'exists:App\Entities\User,id|required',
             'pqrs' => 'exists:App\Entities\Pqrs,id_pqrs|required',
-       ]);
+        ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->messages(), 403);
         }
 
@@ -212,11 +221,11 @@ class PqrsController extends Controller
         $seguimiento->mensaje = $request['mensaje'];
         $seguimiento->hora = Carbon::now()->format('H:i');
 
-        if($seguimiento->save()){
-            $message_pqrs = SeguimientoPqrs::select('seguimiento_pqrs.*','users.name', 'users.apellidos', 'users.rol_id')
-                        ->join('users', 'usuario', '=', 'id')
-                        ->where('id_seguimiento', $seguimiento->id_seguimiento)
-                        ->first();
+        if ($seguimiento->save()) {
+            $message_pqrs = SeguimientoPqrs::select('seguimiento_pqrs.*', 'users.name', 'users.apellidos', 'users.rol_id')
+                ->join('users', 'usuario', '=', 'id')
+                ->where('id_seguimiento', $seguimiento->id_seguimiento)
+                ->first();
 
             $val_rol_user = auth()->user()->rol_id;
             $message_pqrs->iniciales = substr($message_pqrs->name, 0, 1);
@@ -227,20 +236,20 @@ class PqrsController extends Controller
                 // Si el usuario autenticado es admin o vendedor. Se pone como destinatario o addressee al vendedor.
                 if ($val_rol_user == 1 || $val_rol_user == 2) {
                     $message_pqrs->addressee = true;
-                }else{
+                } else {
                     $message_pqrs->addressee = false;
                 }
-            }else if($message_pqrs->rol_id == 3){
+            } else if ($message_pqrs->rol_id == 3) {
                 // Si el usuario autenticado es admin o vendedor. pero el mensaje lo diligencio un cliente
                 if ($val_rol_user == 1 || $val_rol_user == 2) {
                     $message_pqrs->addressee = false;
-                }else{
+                } else {
                     $message_pqrs->addressee = true;
                 }
-            }else{
+            } else {
                 if ($val_rol_user == 1 || $val_rol_user == 2) {
                     $message_pqrs->addressee = true;
-                }else{
+                } else {
                     $message_pqrs->addressee = false;
                 }
             }
@@ -248,14 +257,14 @@ class PqrsController extends Controller
             $response = [
                 'response' => 'success',
                 'status' => 200,
-                'mensaje' => $message_pqrs
+                'mensaje' => $message_pqrs,
             ];
-        }else{
+        } else {
 
             $response = [
                 'response' => 'error',
                 'status' => 403,
-                'message' => 'Error creando el mensaje.'
+                'message' => 'Error creando el mensaje.',
             ];
         }
 
@@ -263,30 +272,31 @@ class PqrsController extends Controller
 
     }
 
-    public function changeState($id, $state){
+    public function changeState($id, $state)
+    {
         $pqrs = Pqrs::find($id);
         if ($state == 'abierto' || $state == 'cerrado') {
             $pqrs->estado = $state;
-        }else{
+        } else {
             $response = [
                 'response' => 'error',
                 'status' => 403,
-                'message' => 'El estado enviado no es valido.'
+                'message' => 'El estado enviado no es valido.',
             ];
             return response()->json($response, $response['status']);
         }
 
-        if($pqrs->save()){
+        if ($pqrs->save()) {
             $response = [
                 'response' => 'success',
                 'status' => 200,
-                'message' => 'Estado asignado de manera correcta.'
+                'message' => 'Estado asignado de manera correcta.',
             ];
-        }else{
+        } else {
             $response = [
                 'response' => 'error',
                 'status' => 403,
-                'message' => 'Error en el cambio de estado.'
+                'message' => 'Error en el cambio de estado.',
             ];
         }
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -10,22 +9,22 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::get('/unauthenticated', function() {
+Route::get('/unauthenticated', function () {
     $response = [
         'response' => 'error',
         'status' => 403,
-        'message' => 'Token vencido o invalido.'
+        'message' => 'Token vencido o invalido.',
     ];
     return response()->json($response, $response['status']);
 })->name('unauthenticated');
 
-Route::group( [ 'middleware' => ['role:administrador'] ], function() {
+Route::group(['middleware' => ['role:administrador']], function () {
     // Route::apiResource('/users', 'UserController');
 });
 
-Route::group( [ 'middleware' => ['permission:create user'] ], function() {
+Route::group(['middleware' => ['permission:create user']], function () {
     #Route::apiResource('/users', 'UserController');
 });
 
@@ -40,14 +39,14 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('login', 'AuthController@login');
     Route::post('signup', 'AuthController@signup');
 
-    Route::group(['middleware' => 'auth:api'], function() {
+    Route::group(['middleware' => 'auth:api'], function () {
         Route::get('logout', 'AuthController@logout');
         Route::get('user', 'AuthController@user');
-        Route::get('/getUserSesion','AuthController@getUserSesion');
+        Route::get('/getUserSesion', 'AuthController@getUserSesion');
     });
 });
 
-Route::group(['middleware' => 'auth:api'], function() {
+Route::group(['middleware' => 'auth:api'], function () {
 
     Route::apiResource('/roles', 'RolController');
     // Route::post('/userdata', 'UserController@userData');
@@ -58,7 +57,7 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::apiResource('/users', 'UserController');
     Route::post('/update-user', 'UserController@updateUser');
     Route::post('/delete-users', 'UserController@destroyUsers');
-    Route::get('/user-rol/{id}','UserController@getForRole');
+    Route::get('/user-rol/{id}', 'UserController@getForRole');
     // Route::post('/delete-user', 'UserController@destroyUsers');
 
     // ADMINISTRADORES
@@ -66,17 +65,17 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::get('/admin/{id}', 'UserController@getAdmin');
 
     //VENDEDORES
-    Route::get('/vendedores','UserController@getVendedores');
-    Route::get('/vendedor/{id}','UserController@getVendedor');
+    Route::get('/vendedores', 'UserController@getVendedores');
+    Route::get('/vendedor/{id}', 'UserController@getVendedor');
     Route::get('/clientes-asignados/{id}', 'UserController@assignedCustomers');
     Route::get('/searchClientes', 'UserController@searchClientes');
     Route::post('/update-vendedor/{id}', 'UserController@updateVendedor');
     Route::get('updateAsignClient/{cliente}/{vendedor}/{action}', 'UserController@updateAsignClient');
 
     // CLIENTES
-    Route::get('/clientes','UserController@getClientes');
-    Route::get('/cliente/{id}','UserController@getCliente');
-    Route::post('/update-cliente/{id}','UserController@updateClient');
+    Route::get('/clientes', 'UserController@getClientes');
+    Route::get('/cliente/{id}', 'UserController@getCliente');
+    Route::post('/update-cliente/{id}', 'UserController@updateClient');
     Route::get('/searchVendedor', 'UserController@searchVendedor');
     Route::get('updateAsignVend/{cliente}/{vendedor}/{action}', 'UserController@updateAsignVend');
     Route::post('newTienda/{cliente}', 'UserController@newTienda');
