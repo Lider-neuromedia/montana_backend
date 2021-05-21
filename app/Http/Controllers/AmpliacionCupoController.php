91e76d27417ec6e769caf4952409c84d4398a651
Jose Nieto <inge1neuro@gmail.com>
2021-05-21 09:53:42 -0500

    Organizando código de controlador AmpliacionCupo

--- app/Http/Controllers/AmpliacionCupoController.php
+++ app/Http/Controllers/AmpliacionCupoController.php
@@ -1,31 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
+
 use App\Entities\AmpliacionCupo;
-use DB;
 use Carbon\Carbon;
+use DB;
+use Illuminate\Http\Request;
 
-class AmpliacionCupoController extends Controller{
-
-    public function index(Request $request){
-
+class AmpliacionCupoController extends Controller
+{
+    public function index(Request $request)
+    {
         if (isset($request['search'])) {
             $search = $request['search'];
-        }else{
+        } else {
             $search = "";
         }
 
-        $solicitudes = AmpliacionCupo::select('id_cupo','codigo_solicitud', 'fecha_solicitud',
-                'vendedor', 'cliente', 'doc_identidad', 'doc_rut', 'doc_camara_com', 'monto',
-                'estado', 'vend.name', 'vend.apellidos')
-                ->join('users as vend', 'vendedor', '=', 'vend.id')
-                ->where('codigo_solicitud', 'like', "%$search%")
-                ->orWhere('vend.name', 'like', "%$search%")
-                ->orWhere('vend.apellidos', 'like', "%$search%")
-                ->orWhere('estado', 'like', "%$search%")
-                ->orWhere('monto', 'like', "%$search%")
-                ->get();
+        $solicitudes = AmpliacionCupo::select('id_cupo', 'codigo_solicitud', 'fecha_solicitud',
+            'vendedor', 'cliente', 'doc_identidad', 'doc_rut', 'doc_camara_com', 'monto',
+            'estado', 'vend.name', 'vend.apellidos')
+            ->join('users as vend', 'vendedor', '=', 'vend.id')
+            ->where('codigo_solicitud', 'like', "%$search%")
+            ->orWhere('vend.name', 'like', "%$search%")
+            ->orWhere('vend.apellidos', 'like', "%$search%")
+            ->orWhere('estado', 'like', "%$search%")
+            ->orWhere('monto', 'like', "%$search%")
+            ->get();
 
         foreach ($solicitudes as $solicitud) {
             $solicitud->doc_identidad = url($solicitud->doc_identidad);
@@ -36,21 +37,23 @@ class AmpliacionCupoController extends Controller{
         $response = [
             'response' => 'success',
             'status' => 200,
-            'solicitudes' => $solicitudes
+            'solicitudes' => $solicitudes,
         ];
 
         return response()->json($response);
     }
 
-    public function getUserSmall($rol_id){
-        $users = DB::table('users')->select('id', 'name', 'apellidos')
-        ->where('rol_id', $rol_id)
-        ->get();
+    public function getUserSmall($rol_id)
+    {
+        $users = DB::table('users')
+            ->select('id', 'name', 'apellidos')
+            ->where('rol_id', $rol_id)
+            ->get();
 
         $response = [
             'response' => 'success',
             'status' => 200,
-            'users' => $users
+            'users' => $users,
         ];
 
         return response()->json($response);
@@ -74,7 +77,8 @@ class AmpliacionCupoController extends Controller{
         return $filename;
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $request->validate([
             'vendedor' => 'required|exists:App\Entities\User,id',
             'cliente' => 'required|exists:App\Entities\User,id',
@@ -94,20 +98,20 @@ class AmpliacionCupoController extends Controller{
 
         // Guardar imagenes.
         $path_solicitud = "storage/solicitudes/{$request['cliente']}/";
-        $solicitud->doc_identidad = $path_solicitud . $this->saveImage($request->file('doc_identidad'), $request['cliente'],'doc_identidad');
+        $solicitud->doc_identidad = $path_solicitud . $this->saveImage($request->file('doc_identidad'), $request['cliente'], 'doc_identidad');
         $solicitud->doc_rut = $path_solicitud . $this->saveImage($request->file('doc_rut'), $request['cliente'], 'doc_rut');
-        $solicitud->doc_camara_com = $path_solicitud . $this->saveImage($request->file('doc_camara_com'), $request['cliente'],'doc_camara_comercio');
+        $solicitud->doc_camara_com = $path_solicitud . $this->saveImage($request->file('doc_camara_com'), $request['cliente'], 'doc_camara_comercio');
 
-        if($solicitud->save()){
+        if ($solicitud->save()) {
             $response = [
                 'response' => 'success',
                 'status' => 200,
             ];
-        }else{
+        } else {
             $response = [
                 'response' => 'error',
                 'status' => 403,
-                'message' => 'Error con la creación de la solicitud. Intente nuevamente'
+                'message' => 'Error con la creación de la solicitud. Intente nuevamente',
             ];
         }
 
@@ -131,25 +135,25 @@ class AmpliacionCupoController extends Controller{
         // Guardar imagenes.
         $path_solicitud = "storage/solicitudes/{$request['cliente']}/";
         if (isset($request['doc_identidad']) && $request['doc_identidad'] != '') {
-            $solicitud->doc_identidad = $path_solicitud . $this->saveImage($request->file('doc_identidad'), $request['cliente'],'doc_identidad');
+            $solicitud->doc_identidad = $path_solicitud . $this->saveImage($request->file('doc_identidad'), $request['cliente'], 'doc_identidad');
         }
         if (isset($request['doc_rut']) && $request['doc_rut'] != '') {
             $solicitud->doc_rut = $path_solicitud . $this->saveImage($request->file('doc_rut'), $request['cliente'], 'doc_rut');
         }
         if (isset($request['doc_camara_com']) && $request['doc_camara_com'] != '') {
-            $solicitud->doc_camara_com = $path_solicitud . $this->saveImage($request->file('doc_camara_com'), $request['cliente'],'doc_camara_comercio');
+            $solicitud->doc_camara_com = $path_solicitud . $this->saveImage($request->file('doc_camara_com'), $request['cliente'], 'doc_camara_comercio');
         }
 
-        if($solicitud->save()){
+        if ($solicitud->save()) {
             $response = [
                 'response' => 'success',
                 'status' => 200,
             ];
-        }else{
+        } else {
             $response = [
                 'response' => 'error',
                 'status' => 403,
-                'message' => 'Error con la actualización de la solicitud. Intente nuevamente'
+                'message' => 'Error con la actualización de la solicitud. Intente nuevamente',
             ];
         }
 
@@ -157,17 +161,17 @@ class AmpliacionCupoController extends Controller{
 
     }
 
-    public function changeState($solicitud, $estado){
+    public function changeState($solicitud, $estado)
+    {
         $solicitud_db = AmpliacionCupo::find($solicitud);
         $solicitud_db->estado = $estado;
 
-        if($solicitud_db->save()){
+        if ($solicitud_db->save()) {
             $response = ['response' => 'success', 'status' => 200];
-        }else{
+        } else {
             $response = ['response' => 'error', 'status' => 403, 'message' => 'Error en el cambio de estado.'];
         }
 
         return response()->json($response);
     }
-
 }
